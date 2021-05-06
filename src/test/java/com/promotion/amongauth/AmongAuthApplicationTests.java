2205435a45922293b1ca671d1b075f5ef1ff4246
sunrabbit123 <qudwls185@naver.com>
2021-05-06 17:53:31 +0900

    initial commit

---
+++ src/test/java/com/promotion/amongauth/AmongAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promotion.amongauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmongAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
