2518e8ee30e44bf1992566b3c7798f059d7e6183
Xylope <XylopeOfficial@gmail.com>
2021-05-07 10:03:18 +0900

    데이터 액세스 계층 일부 구현 - Feat Repositories using JpaRepository Interface

---
+++ among-api/src/main/java/com/promotion/amongauth/amongapi/dataaccess/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.promotion.amongauth.amongapi.dataaccess.repositories;
+
+import com.promotion.amongauth.amongapi.dataaccess.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByName(String name);
+    int findByGradeAndClazzAndNumber(int grade, int clazz, int number);
+
+    int countByGeneration(int generation);
+    int countByGrade(int grade);
+    int countByClazz(int clazz);
+}
